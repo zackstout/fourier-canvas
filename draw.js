00d26db3d412d33ffe785309a57dad1807a63a38
Zack Stout <zackstout@gmail.com>
2018-08-12 16:03:10 -0500

    adding a second canvas element not so hard

--- draw.js
+++ draw.js
@@ -1,7 +1,8 @@
 
 var circs = [];
+var counter = 0;
 
-function generateCircles(n) {
+function generateCircles(n, p) {
   var size = 80;
 
   for (var i=0; i < n; i++) {
@@ -10,38 +11,93 @@ function generateCircles(n) {
     } else {
       center = prev_center;
     }
-    var circ = new Circle(center.x, center.y, size, Math.random() * 4);
+    var circ = new Circle(center.x, center.y, size, Math.random() * 4, p);
     circs.push(circ);
     prev_center = center;
     size *= 0.6;
   }
 }
 
-function setup() {
-  createCanvas(1000, 600);
-  background('lightGrey');
-  generateCircles(4);
+new p5(topSketch, 'top');
+new p5(btmSketch, 'btm');
+
+function btmSketch(p) {
+  p.setup = function() {
+    p.createCanvas(1000, 400);
+    p.background('lightblue');
+    p.line(10, 200, 990, 200);
+  };
+
+  p.draw = function() {
+    p.background('lightblue');
+  };
 }
 
-function draw() {
-  background('lightGray');
-  // Going from largest to smallest, inner to outer:
-  for (var i=0; i < circs.length; i++) {
-    var c = circs[i];
-    c.pointer = c.findPointer();
-    if (i > 0) {
-      c.x = next_center.x;
-      c.y = next_center.y;
+function topSketch(p) {
+  p.setup = function() {
+    p.createCanvas(1000, 1000);
+    p.background('lightGrey');
+    generateCircles(4, p);
+  };
+
+  p.draw = function() {
+    counter ++;
+    p.background('lightGray');
+    // Going from largest to smallest, inner to outer:
+    for (var i=0; i < circs.length; i++) {
+      var c = circs[i];
+      c.pointer = c.findPointer();
+      if (i > 0) {
+        c.x = next_center.x;
+        c.y = next_center.y;
+      }
+      next_center = c.pointer;
+      c.draw();
+      c.angle += 0.02 * c.v;
     }
-    next_center = c.pointer;
-    c.draw();
-    c.angle += 0.02 * c.v;
-  }
 
+  };
 }
+
+
+//
+// function setup() {
+//   createCanvas(1000, 1000);
+//   background('lightGrey');
+//   generateCircles(4);
+// }
+//
+// function draw() {
+//   counter ++;
+//   background('lightGray');
+//   // Going from largest to smallest, inner to outer:
+//   for (var i=0; i < circs.length; i++) {
+//     var c = circs[i];
+//     c.pointer = c.findPointer();
+//     if (i > 0) {
+//       c.x = next_center.x;
+//       c.y = next_center.y;
+//     }
+//     next_center = c.pointer;
+//     c.draw();
+//     c.angle += 0.02 * c.v;
+//   }
+//
+//   // time axis:
+//   stroke('black');
+//   line(10, 800, 990, 800);
+//
+//   if (counter % 8 == 0) {
+//     // console.log(circs[circs.length - 1].y);
+//
+//
+//   }
+//
+//
+// }
 
 // Takes center, radius and velocity:
-function Circle(x, y, r, v) {
+function Circle(x, y, r, v, p) {
   this.x = x;
   this.y = y;
   this.r = r;
@@ -51,24 +107,26 @@ function Circle(x, y, r, v) {
 
   this.findPointer = function() {
     return {
-      x: cos(this.angle) * this.r,
-      y: sin(this.angle) * this.r
+      x: p.cos(this.angle) * this.r,
+      y: p.sin(this.angle) * this.r
     };
   };
 
   this.draw = function() {
-    push();
-    translate(this.x, this.y);
-    noFill();
-    stroke('black');
-    ellipse(0, 0, this.r * 2);
+    // push();
+    // Yeah this is imperfect without pushing but....we'll just make a new canvas.
+    p.translate(this.x, this.y);
+    p.noFill();
+    p.stroke('black');
+    p.ellipse(0, 0, this.r * 2);
     // stroke();
 
-    stroke('tomato');
-    line(0, 0, this.pointer.x, this.pointer.y);
+    p.stroke('tomato');
+    p.line(0, 0, this.pointer.x, this.pointer.y);
 
-    fill('steelblue');
-    noStroke();
-    ellipse(this.pointer.x, this.pointer.y, 5);
+    p.fill('steelblue');
+    p.noStroke();
+    p.ellipse(this.pointer.x, this.pointer.y, 5);
+    // pop();
   };
 }
