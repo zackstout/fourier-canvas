1886b106223d8fbbe68721c3599d8d04f60ddf4a
Zack Stout <zackstout@gmail.com>
2018-08-12 16:43:57 -0500

    cleaning up code

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Creating an animation of a crucial step in the Fourier transformation to deepen my mathematical understanding of it.
 
 ## Animation:
+The main idea is that the signal being charted on the lower canvas is composed out of the perfect sinusoidal signals represented by each spinning circle. The purpose of the Fourier transform is to decompose signals into their simple sinusoidal elements.
+
 ![](https://media.giphy.com/media/1kJyKBc7NuLHK8Fg7r/giphy.gif)
 
 ## Next Steps:
--- draw.js
+++ draw.js
@@ -1,61 +1,13 @@
 
 var circs = [];
-var counter = 0; // No longer using this.
 var total_history = []; // yeah, history was a keyword.
 var time = 0;
-
-// Get rid of magic nums...
 var w1, h1, w2, h2;
-
-
-function generateCircles(n, p) {
-  var size = 80;
-
-  for (var i=0; i < n; i++) {
-    if (i === 0) {
-      center = {x: 500, y: 250};
-    } else {
-      center = prev_center;
-    }
-    var circ = new Circle(center.x, center.y, size, Math.random() * 4, p);
-    circs.push(circ);
-    prev_center = center;
-    size *= 0.6;
-  }
-}
 
 new p5(topSketch, 'top');
 new p5(btmSketch, 'btm');
 
-function btmSketch(p) {
-  p.setup = function() {
-    w1 = 1000;
-    h1 = 400;
-    p.createCanvas(w1, h1);
-    // p.background('lightblue');
-    // p.line(10, h1/2, w1 - 10, h1/2);
-  };
-
-  p.draw = function() {
-    // p.background('lightblue');
-
-
-    // if (counter % 10 === 0) {
-    p.background('lightblue');
-    p.stroke('black');
-    p.line(10, h1/2, w1 - 10, h1/2);
-    // console.log(total_history);
-    total_history.forEach(function(c) {
-      p.noStroke();
-      p.fill('steelblue');
-      p.ellipse(10 + c.x , h1/2 - c.y * 1.5, 4);
-    });
-    // }
-  };
-}
-
-
-
+// ====================================================================================
 
 function topSketch(p) {
   p.setup = function() {
@@ -67,7 +19,6 @@ function topSketch(p) {
   };
 
   p.draw = function() {
-    // counter ++;
     p.background('lightGray');
     p.stroke('gray');
     p.line(10, h2/2, w2 - 10, h2/2);
@@ -94,17 +45,54 @@ function topSketch(p) {
       return s + c.y;
     }, -h2/2);
 
-    // if (counter % 1 === 0) {
     total_history.push({
       x: time,
       y: final_y
     });
     time ++;
-    // }
-
   };
 }
 
+// ====================================================================================
+
+function btmSketch(p) {
+  p.setup = function() {
+    w1 = 1000;
+    h1 = 400;
+    p.createCanvas(w1, h1);
+  };
+
+  p.draw = function() {
+    p.background('lightblue');
+    p.stroke('black');
+    p.line(10, h1/2, w1 - 10, h1/2);
+    total_history.forEach(function(c) {
+      p.noStroke();
+      p.fill('steelblue');
+      p.ellipse(10 + c.x , h1/2 - c.y * 1.5, 4);
+    });
+  };
+}
+
+// ====================================================================================
+
+function generateCircles(n, p) {
+  var size = 80;
+
+  for (var i=0; i < n; i++) {
+    if (i === 0) {
+      center = {x: 500, y: 250};
+    } else {
+      center = prev_center;
+    }
+    var circ = new Circle(center.x, center.y, size, Math.random() * 4, p);
+    circs.push(circ);
+    prev_center = center;
+    size *= 0.6;
+  }
+}
+
+// ====================================================================================
 
 // Takes center, radius and velocity:
 function Circle(x, y, r, v, p) {
@@ -123,7 +111,7 @@ function Circle(x, y, r, v, p) {
   };
 
   this.draw = function(bool=false) {
-    // Yeah this is imperfect without pushing but....we'll just make a new canvas.
+    // Yeah this is imperfect without pushing but....we'll just make a new canvas. We end up using reduce to get around this problem.
     p.translate(this.x, this.y);
     p.noFill();
     p.stroke('black');
